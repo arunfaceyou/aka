38bfea63f2d7fe405bc9bb06912de9563d2d2909
arun chauhan <arunfaceyou@gmail.com>
2020-10-30 09:34:02 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', function (req, res) {
-    res.send("Hello World!");
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+ 
+io.on('connection', (socket) => {
+ 
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.username, event: 'left'});   
+  });
+ 
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});    
+  });
+  
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
+  });
 });
-
-app.listen(port);
+ 
+var port = process.env.PORT || 3001;
+ 
+server.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
